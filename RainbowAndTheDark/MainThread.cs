cafe9705fda67b8b4e08bfababbdb8081f9b7422
NickLatkovich <nicklatkovich@gmail.com>
2017-05-19 11:52:35 +0300

    Init

---
+++ RainbowAndTheDark/MainThread.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace RainbowAndTheDark {
+    public class MainThread : Game {
+        GraphicsDeviceManager Graphics;
+        SpriteBatch SpriteBatch;
+        KeyboardState Keyboard;
+        KeyboardState KeyboardPrevious = SimpleUtils.GetKeyboardState( );
+
+        public MainThread( ) {
+            Graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+        }
+
+        protected override void Initialize( ) {
+            // TODO: initialization logic
+
+            base.Initialize( );
+        }
+
+        protected override void LoadContent( ) {
+            SpriteBatch = new SpriteBatch(GraphicsDevice);
+
+            // TODO: load content
+        }
+
+        protected override void UnloadContent( ) {
+            // TODO: Unload any non ContentManager content
+        }
+
+        protected override void Update(GameTime time) {
+            Keyboard = SimpleUtils.GetKeyboardState( );
+            if (Keyboard.IsKeyDown(Keys.Escape)) {
+                Exit( );
+            }
+
+            // TODO: update logic
+
+            KeyboardPrevious = Keyboard;
+            base.Update(time);
+        }
+
+        protected override void Draw(GameTime time) {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            // TODO: drawing code
+
+            base.Draw(time);
+        }
+    }
+}
---
+++ RainbowAndTheDark/SimpleUtils.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RainbowAndTheDark {
+    public static class SimpleUtils {
+
+        public static KeyboardState GetKeyboardState( ) {
+            return Keyboard.GetState( );
+        }
+
+    }
+}
